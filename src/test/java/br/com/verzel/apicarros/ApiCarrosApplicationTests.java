1300b38fd68bbfbfb20cb4194d1dad43ee5d041c
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-01-10 12:27:13 -0300

    Projeto backend criado

---
+++ src/test/java/br/com/verzel/apicarros/ApiCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.verzel.apicarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
