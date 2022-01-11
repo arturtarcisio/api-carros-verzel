594203617fd66c0202eca5b22837b63aa8cbd5d2
Artur Tarcisio <arturtarcisio1@gmail.com>
2022-01-11 12:16:28 -0300

    Criando classe de internacionalizacao e melhorando validacao das entidades no backend

--- backend/src/main/java/br/com/verzel/apicarros/entities/Carro.java
+++ backend/src/main/java/br/com/verzel/apicarros/entities/Carro.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -23,16 +20,20 @@ public class Carro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
+    @Column(nullable = false, length = 100)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
-    @NotEmpty
+    @Column(nullable = false, length = 50)
+    @NotEmpty(message = "{campo.marca.obrigatorio}")
     private String marca;
 
-    @NotEmpty
+    @Column(nullable = false, length = 30)
+    @NotEmpty(message = "{campo.modelo.obrigatorio}")
     private String modelo;
 
-    @NotNull
+    @Column(nullable = false)
+    @NotNull(message = "{campo.valor.obrigatorio}")
     private Double valor;
 
 }
--- backend/src/main/java/br/com/verzel/apicarros/services/impl/CarroServiceImpl.java
+++ backend/src/main/java/br/com/verzel/apicarros/services/impl/CarroServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
